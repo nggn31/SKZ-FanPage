637df261ed961687f2584e1198cfc041aaf6c812
nggn31 <159208505+nggn31@users.noreply.github.com>
2024-04-11 13:46:56 -0700

    third commit

--- js/all-in-songs.js
+++ js/all-in-songs.js
@@ -2,21 +2,21 @@
 
 const songs = [
     {
-      path: "../music/all-in/1.mp3",
+      path: "music/all-in/1.mp3",
       displayName: "All in",
-      cover: "../css/img/all-in.jpeg",
+      cover: "css/img/all-in.jpeg",
       artist: "Stray Kids",
     },
     {
-        path: "../music/all-in/2.mp3",
+        path: "music/all-in/2.mp3",
         displayName: "FAM.Korean ver",
-        cover: "../css/img/all-in.jpeg",
+        cover: "css/img/all-in.jpeg",
         artist: "Stray Kids",
     },
     {
-      path: "../music/all-in/3.mp3",
+      path: "music/all-in/3.mp3",
         displayName: "One Day",
-        cover: "../css/img/all-in.jpeg",
+        cover: "css/img/all-in.jpeg",
         artist: "Stray Kids",
       },
   ];
--- js/mixtape-songs.js
+++ js/mixtape-songs.js
@@ -2,39 +2,39 @@
 
 const songs = [
   {
-    path: "../music/1.mp3",
+    path: "music/1.mp3",
     displayName: "Hellevator",
-    cover: "../css/img/mixtape.jpg",
+    cover: "css/img/mixtape.jpg",
     artist: "Stray Kids",
   },
   {
-    path: "../music/2.mp3",
+    path: "music/2.mp3",
     displayName: "YAYAYA",
-    cover: "../css/img/mixtape.jpg",
+    cover: "css/img/mixtape.jpg",
     artist: "Stray kids",
   },
   {
-    path: "../music/3.mp3",
+    path: "music/3.mp3",
     displayName: "School Life",
-    cover: "../css/img/mixtape.jpg",
+    cover: "css/img/mixtape.jpg",
     artist: "Stray kids",
   },
   {
-    path: "../music/4.mp3",
+    path: "music/4.mp3",
     displayName: "Grr",
-    cover: "../css/img/mixtape.jpg",
+    cover: "css/img/mixtape.jpg",
     artist: "Stray kids",
   },
   {
-      path: "../music/5.mp3",
+      path: "music/5.mp3",
       displayName: "GLOW",
-      cover: "../css/img/mixtape.jpg",
+      cover: "css/img/mixtape.jpg",
       artist: "Stray kids",
   },
   {
-      path: "../music/6.mp3",
+      path: "music/6.mp3",
       displayName: "4419",
-      cover: "../css/img/mixtape.jpg",
+      cover: "css/img/mixtape.jpg",
       artist: "Stray kids",
   },
 ];
